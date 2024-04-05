5558a0c522fe0bd873afdba05ae479e9c6135448
Villeman <villeman.arm@gmail.com>
2024-04-05 20:49:51 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import NewComponent from './components/NewComponent.vue'
+</script>
+
+<template>
+  <NewComponent msg="You did it!" />
+
+</template>
+
+<style scoped lang="sass">
+
+</style>
---
+++ src/components/NewComponent.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { reactive, ref, computed, onMounted, onUpdated, watch } from 'vue'
+
+defineProps<{
+	msg: string;
+}>();
+
+</script>
+
+<template>
+
+	<div class="">
+		<h1>{{ msg }}</h1>
+	</div>
+
+</template>
+
+<style scoped lang="sass">
+	@import '@/assets/styles/constants.sass'
+
+</style>
+
+
+
+
+
